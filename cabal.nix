6cc09048388e4f4c5a5e6fe299aace63ea71ddc8
Ning Zhang <n.zhang.hp.au@gmail.com>
2019-01-15 22:45:36 +1100

    Updated to cabal2nix project

---
+++ cabal.nix
@@ -0,0 +1,23 @@
+{ mkDerivation, attoparsec, base, base58-bytestring
+, base64-bytestring, bytestring, cryptonite, hex, hpack, io-streams
+, memory, optparse-applicative, stdenv
+}:
+mkDerivation {
+  pname = "hs-multihash";
+  version = "0.2.1";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    attoparsec base base58-bytestring base64-bytestring bytestring
+    cryptonite hex io-streams memory
+  ];
+  libraryToolDepends = [ hpack ];
+  executableHaskellDepends = [
+    base bytestring io-streams optparse-applicative
+  ];
+  preConfigure = "hpack";
+  homepage = "https://github.com/MatrixAI/hs-multihash#readme";
+  description = "Multihash library and CLI executable";
+  license = stdenv.lib.licenses.bsd3;
+}
